17422adc3ff415f8111c7b827fffe1fc68fbd927
Zhaolifang1 <2322344659@qq.com>
2017-11-15 20:37:59 +0800

    登录注册

---
+++ app/src/main/java/com/example/jd/mine/model/MyIModel.java
@@ -0,0 +1,12 @@
+package com.example.jd.mine.model;
+
+/**
+ * @author 赵利芳
+ *         类的作用：
+ * @date
+ */
+
+public interface MyIModel {
+    //联网登陆
+    void HttpLogin(String mobile, String password);
+}
